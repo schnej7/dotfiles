33ae8871b764968dad15db7e30c05530d6dbe892
Jerry Schneider <schnej7@gmail.com>
2026-07-29 09:35:32 -0400

    fix(bash): fixed errors on source

--- bash/osx/osx.bash
+++ bash/osx/osx.bash
@@ -1,7 +1,9 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# Source bash completion, suppressing 'nosort' errors from newer completion
+# scripts that require Bash 4.4+ (macOS ships with Bash 3.2)
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+  . $(brew --prefix)/etc/bash_completion 2> >(grep -v 'nosort: invalid option' >&2)
 fi
 
 # add colors to ls
